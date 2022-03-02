dbf7fb026ff1ba32e886f617e87ba25983a8f935
Mateusz Lichota <mateusz@lichota.net>
2022-03-02 15:01:48 +0000

    rewrite detect.py a little

--- CVC-YOLOv3/detect.py
+++ CVC-YOLOv3/detect.py
@@ -12,14 +12,17 @@ import subprocess
 import shutil
 import cv2
 
+from pathlib import Path
+
 import torch
+import torch.cuda
+import torch.backends
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from tensorboardX import SummaryWriter
 
 from PIL import Image, ImageDraw
 
-import torchvision
+import torchvision.transforms.functional
 from models import Darknet
 from utils.datasets import ImageLabelDataset
 from utils.nms import nms
@@ -32,111 +35,141 @@ warnings.filterwarnings("ignore")
 
 detection_tmp_path = "/tmp/detect/"
 
+from dataclasses import dataclass
 
-def main(target_path,output_path,weights_path,model_cfg,conf_thres,nms_thres,xy_loss,wh_loss,no_object_loss,object_loss,vanilla_anchor):
-
-    cuda = torch.cuda.is_available()
-    device = torch.device('cuda:0' if cuda else 'cpu')
-    random.seed(0)
-    torch.manual_seed(0)
-    if cuda:
-        torch.cuda.manual_seed(0)
-        torch.cuda.manual_seed_all(0)
-        torch.backends.cudnn.benchmark = True
-        torch.cuda.empty_cache()
-    model = Darknet(config_path=model_cfg,xy_loss=xy_loss,wh_loss=wh_loss,no_object_loss=no_object_loss,object_loss=object_loss,vanilla_anchor=vanilla_anchor)
-
-    # Load weights
-    model.load_weights(weights_path, model.get_start_weight_dim())
-    model.to(device, non_blocking=True)
-
-    detect(target_path,
-           output_path,
-           model,
-           device=device,
-           conf_thres=conf_thres,
-           nms_thres=nms_thres)
-
-def single_img_detect(target_path,output_path,mode,model,device,conf_thres,nms_thres):
-
-    img = Image.open(target_path).convert('RGB')
-    w, h = img.size
-    new_width, new_height = model.img_size()
-    pad_h, pad_w, ratio = calculate_padding(h, w, new_height, new_width)
-    img = torchvision.transforms.functional.pad(img, padding=(pad_w, pad_h, pad_w, pad_h), fill=(127, 127, 127), padding_mode="constant")
-    img = torchvision.transforms.functional.resize(img, (new_height, new_width))
-
-    bw = model.get_bw()
-    if bw:
-        img = torchvision.transforms.functional.to_grayscale(img, num_output_channels=1)
-
-    img = torchvision.transforms.functional.to_tensor(img)
-    img = img.unsqueeze(0)
-    
-    with torch.no_grad():
-        model.eval()
-        img = img.to(device, non_blocking=True)
-        # output,first_layer,second_layer,third_layer = model(img)
-        output = model(img)
-
-
-        for detections in output:
-            detections = detections[detections[:, 4] > conf_thres]
-            box_corner = torch.zeros((detections.shape[0], 4), device=detections.device)
-            xy = detections[:, 0:2]
-            wh = detections[:, 2:4] / 2
-            box_corner[:, 0:2] = xy - wh
-            box_corner[:, 2:4] = xy + wh
-            probabilities = detections[:, 4]
-            nms_indices = nms(box_corner, probabilities, nms_thres)
-            main_box_corner = box_corner[nms_indices]
-            if nms_indices.shape[0] == 0:  
-                continue
-        img_with_boxes = Image.open(target_path)
-        draw = ImageDraw.Draw(img_with_boxes)
-        w, h = img_with_boxes.size
-
-        for i in range(len(main_box_corner)):
-            x0 = main_box_corner[i, 0].to('cpu').item() / ratio - pad_w
-            y0 = main_box_corner[i, 1].to('cpu').item() / ratio - pad_h
-            x1 = main_box_corner[i, 2].to('cpu').item() / ratio - pad_w
-            y1 = main_box_corner[i, 3].to('cpu').item() / ratio - pad_h 
-            draw.rectangle((x0, y0, x1, y1), outline="red")
-
-        if mode == 'image':
-            img_with_boxes.save(os.path.join(output_path,target_path.split('/')[-1]))
-            return os.path.join(output_path,target_path.split('/')[-1])
+class Detector:
+    def __init__(self, 
+        target_path,
+        output_path,
+        weights_path,
+        model_cfg,
+        conf_thres,
+        nms_thres,
+        xy_loss,
+        wh_loss,
+        no_object_loss,
+        object_loss,
+        vanilla_anchor
+    ) -> None:
+        if False: #torch.cuda.is_available():
+            self.device = torch.device('cuda:0')
+            self.setup_cuda()
         else:
-            img_with_boxes.save(target_path)
-            return target_path
-
-def detect(target_path,
-           output_path,
-           model,
-           device,
-           conf_thres,
-           nms_thres):
-
-        target_filepath = target_path
-
-        img_formats = ['.jpg', '.jpeg', '.png', '.tif']
-        vid_formats = ['.mov', '.avi', '.mp4']
-
-        mode = None
-
-        if os.path.splitext(target_filepath)[-1].lower() in img_formats:
-            mode = 'image'
+            self.device = torch.device('cpu')
         
-        elif os.path.splitext(target_filepath)[-1].lower() in vid_formats:
-            mode = 'video'
+        random.seed(0)
+        torch.manual_seed(0)
         
+        self.model = Darknet(config_path=model_cfg,xy_loss=xy_loss,wh_loss=wh_loss,no_object_loss=no_object_loss,object_loss=object_loss,vanilla_anchor=vanilla_anchor)
+
+        # Load weights
+        print('Loading weights')
+        self.model.load_weights(weights_path, self.model.get_start_weight_dim())
+
+        print('Moving to device')
+        print(self.device)
+        self.model.to(self.device, non_blocking=True)
+
+        print('got here')
+        self.detect(Path(target_path),
+            output_path,
+            conf_thres=conf_thres,
+            nms_thres=nms_thres)
+
+    def setup_cuda(self):
+        torch.manual_seed(0)
+        # torch.backends.cudnn.benchmark = True
+        torch.cuda.empty_cache()
+
+
+    def single_img_detect(self,
+        target_path,
+        output_path,
+        mode,
+        conf_thres,
+        nms_thres
+    ):
+
+        img = Image.open(target_path).convert('RGB')
+        w, h = img.size
+        new_width, new_height = self.model.img_size()
+        pad_h, pad_w, ratio = calculate_padding(h, w, new_height, new_width)
+        img = torchvision.transforms.functional.pad(img, padding=(pad_w, pad_h, pad_w, pad_h), fill=127, padding_mode="constant")
+        img = torchvision.transforms.functional.resize(img, (new_height, new_width))
+
+        bw = self.model.get_bw()
+        if bw:
+            img = torchvision.transforms.functional.to_grayscale(img, num_output_channels=1)
+
+        img = torchvision.transforms.functional.to_tensor(img)
+        img = img.unsqueeze(0)
+        
+        with torch.no_grad():
+            self.model.eval()
+            img = img.to(self.device, non_blocking=True)
+            # output,first_layer,second_layer,third_layer = model(img)
+            output = self.model(img)
+
+
+            for detections in output:
+                detections = detections[detections[:, 4] > conf_thres]
+                box_corner = torch.zeros((detections.shape[0], 4), device=detections.device)
+                xy = detections[:, 0:2]
+                wh = detections[:, 2:4] / 2
+                box_corner[:, 0:2] = xy - wh
+                box_corner[:, 2:4] = xy + wh
+                probabilities = detections[:, 4]
+                nms_indices = nms(box_corner, probabilities, nms_thres)
+                main_box_corner = box_corner[nms_indices]
+                if nms_indices.shape[0] == 0:  
+                    continue
+            img_with_boxes = Image.open(target_path)
+            draw = ImageDraw.Draw(img_with_boxes)
+            w, h = img_with_boxes.size
+
+            for i in range(len(main_box_corner)):
+                x0 = main_box_corner[i, 0].to('cpu').item() / ratio - pad_w
+                y0 = main_box_corner[i, 1].to('cpu').item() / ratio - pad_h
+                x1 = main_box_corner[i, 2].to('cpu').item() / ratio - pad_w
+                y1 = main_box_corner[i, 3].to('cpu').item() / ratio - pad_h 
+                draw.rectangle((x0, y0, x1, y1), outline="red")
+
+            if mode == 'image':
+                img_with_boxes.save(os.path.join(output_path,target_path.name))
+                return os.path.join(output_path,target_path.name)
+            else:
+                img_with_boxes.save(target_path)
+                return target_path
+
+    def draw_bounding_boxes(self, image):
+        pass
+
+    def detect(self,
+        target_filepath: Path,
+        output_path,
+        conf_thres,
+        nms_thres
+    ):
+        mode_by_extension = {
+            '.jpg': 'image',
+            '.jpeg': 'image',
+            '.png': 'image',
+            '.tif': 'image',
+            '.mov': 'video',
+            '.mp4': 'video',
+            '.avi': 'video',
+        }
+        
+
+        extension = target_filepath.suffix.lower()
+        mode = mode_by_extension[extension]
         print("Detection Mode is: " + mode)
 
-        raw_file_name = target_filepath.split('/')[-1].split('.')[0].split('_')[-4:]
-        raw_file_name = '_'.join(raw_file_name)
         
+        raw_file_name = target_filepath.stem
+
         if mode == 'image':
-            detection_path = single_img_detect(target_path=target_filepath,output_path=output_path,mode=mode,model=model,device=device,conf_thres=conf_thres,nms_thres=nms_thres)
+            detection_path = self.single_img_detect(target_path=target_filepath,output_path=output_path,mode=mode,conf_thres=conf_thres,nms_thres=nms_thres)
 
             print(f'Please check output image at {detection_path}')
 
@@ -175,7 +208,7 @@ def detect(target_path,
             for i in tqdm(files,desc='Doing Single Image Detection'):
                 filename=detection_tmp_path + i
                 
-                detection_path = single_img_detect(target_path=filename,output_path=output_path,mode=mode,model=model,device=device,conf_thres=conf_thres,nms_thres=nms_thres)
+                detection_path = self.single_img_detect(target_path=filename,output_path=output_path,mode=mode,conf_thres=conf_thres,nms_thres=nms_thres)
                 #reading each files
                 img = cv2.imread(detection_path)
                 height, width, layers = img.shape
@@ -202,12 +235,12 @@ if __name__ == '__main__':
         arg_group.add_argument('--' + name, dest=name, action='store_true', help=help)
         arg_group.add_argument('--no_' + name, dest=name, action='store_false', help=("Do not " + help))
         parser.set_defaults(**{name:default})
-    parser.add_argument('--model_cfg', type=str, default='model_cfg/yolo_baseline.cfg')
-    parser.add_argument('--target_path', type=str, help='path to target image/video')
-    parser.add_argument('--output_path', type=str, default="outputs/visualization/")
-    parser.add_argument('--weights_path', type=str, help='path to weights file')
-    parser.add_argument('--conf_thres', type=float, default=0.8, help='object confidence threshold')
-    parser.add_argument('--nms_thres', type=float, default=0.25, help='IoU threshold for non-maximum suppression')
+    parser.add_argument('--model-cfg', type=str, default='model_cfg/yolo_baseline.cfg')
+    parser.add_argument('--target-path', type=str, help='path to target image/video')
+    parser.add_argument('--output-path', type=str, default="outputs/visualization/")
+    parser.add_argument('--weights-path', type=str, help='path to weights file')
+    parser.add_argument('--conf-thres', type=float, default=0.8, help='object confidence threshold')
+    parser.add_argument('--nms-thres', type=float, default=0.25, help='IoU threshold for non-maximum suppression')
 
     add_bool_arg('vanilla_anchor', default=False, help="whether to use vanilla anchor boxes for training")
     ##### Loss Constants #####
@@ -218,7 +251,9 @@ if __name__ == '__main__':
 
     opt = parser.parse_args()
 
-    main(target_path=opt.target_path,
+    print('got here')
+
+    d = Detector(target_path=opt.target_path,
          output_path=opt.output_path,
          weights_path=opt.weights_path,
          model_cfg=opt.model_cfg,
