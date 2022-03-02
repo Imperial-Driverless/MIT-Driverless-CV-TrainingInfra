5ae185031ce2a40a985f0f7084319457cf986bf8
PietroVitiello <vitiellocollegiosancarlo@gmail.com>
2022-03-02 14:16:05 +0000

    added folder where to copy

--- CVC-YOLOv3/getRequiredData.py
+++ CVC-YOLOv3/getRequiredData.py
@@ -2,7 +2,7 @@ import os
 
 print("Downloading Training Dataset")
 os.system("gsutil cp -p gs://mit-driverless-open-source/YOLO_Dataset.zip ./dataset/")
-os.system("unzip dataset/YOLO_Dataset.zip -d ./dataset/")
+os.system("unzip dataset/YOLO_Dataset.zip -d ./dataset/ ./")
 os.system("rm YOLO_Dataset.zip")
 
 print("Downloading YOLOv3 Sample Weights")
