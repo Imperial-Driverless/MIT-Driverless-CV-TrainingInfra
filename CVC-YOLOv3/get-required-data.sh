6831bbe16c612c811b800880193efb896ff782fe
PietroVitiello <vitiellocollegiosancarlo@gmail.com>
2022-05-06 18:38:26 +0100

    fixed get required data

--- CVC-YOLOv3/get-required-data.sh
+++ CVC-YOLOv3/get-required-data.sh
@@ -2,7 +2,7 @@
 echo "Downloading Training Dataset"
 gsutil cp -p gs://mit-driverless-open-source/YOLO_Dataset.zip ./dataset/
 unzip dataset/YOLO_Dataset.zip -d ./dataset/
-rm YOLO_Dataset.zip
+rm dataset/YOLO_Dataset.zip
 
 echo "Downloading YOLOv3 pretrained Weights"
 gsutil cp -p  gs://mit-driverless-open-source/pretrained_yolo.weights ./yolo_weights/
