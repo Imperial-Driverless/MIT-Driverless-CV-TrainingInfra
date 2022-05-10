5b9bfd1c6905600c5fbd5e3454e59240d56983ae
lavinama <70475094+lavinama@users.noreply.github.com>
2022-05-10 18:23:31 +0100

    Change way of cropping

--- CVC-YOLOv3/crop_image.py
+++ CVC-YOLOv3/crop_image.py
@@ -1,4 +1,12 @@
+from PIL import Image
+import numpy
 
-def crop_image(img_with_boxes, x0, y0, x1, y1):
-    cropped_img = img_with_boxes[y0:y1, x0:x1]
-    return cropped_img
+"""def crop_image(target_path, x0, y0, x1, y1):
+    img = Image.open(target_path)
+    # area = (x0, y0, x1, y1)
+    # return img.crop(area)
+    pix = numpy.array(img)
+    crop = pix[x0:x1, y0:y1]
+    PIL_image = Image.fromarray(crop.astype('uint8'), 'RGB')
+    return PIL_image
+"""
